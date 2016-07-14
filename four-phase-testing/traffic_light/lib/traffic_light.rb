0e9a68140d04f48a809139c88027b2e06bf616d7
Sophie Gill <sophie-gill@hotmail.com>
2016-07-14 10:01:09 +0100

    adding state change from stop to wait

--- four-phase-testing/traffic_light/lib/traffic_light.rb
+++ four-phase-testing/traffic_light/lib/traffic_light.rb
@@ -1,10 +1,15 @@
 class TrafficLight
   #the awesome code goes here
-  attr_reader :state, :next
+  attr_reader :state, :next_state
 
   def initialize(state = :stop, next_state = :wait)
     @state = state
-    @next = next_state
+    @next_state = next_state
+  end
+
+  def state_change
+    @state = next_state
+    @next_state = :go
   end
 
 end
--- four-phase-testing/traffic_light/spec/traffic_light_spec.rb
+++ four-phase-testing/traffic_light/spec/traffic_light_spec.rb
@@ -16,11 +16,23 @@ describe TrafficLight do
     it 'is set to "stop" by default' do
       expect(subject.state).to(eq(:stop))
     end
+    #it 'uses the next value to set state' do
+     #subject.state_change
+     #expect(subject.state).to(eq(subject.next_state))
+    #end
+   it 'changes to :wait after :stop' do
+     subject.state_change
+     expect(subject.state).to(eq(:wait))
+   end
   end
 
-  describe '#next' do
+  describe '#next_state' do
     it 'is set to :wait by default' do
-      expect(subject.next).to(eq(:wait))
+      expect(subject.next_state).to(eq(:wait))
+    end
+    it 'changes to :go when state has changed from stop to wait' do
+      subject.state_change
+      expect(subject.next_state).to(eq(:go))
     end
   end
 
