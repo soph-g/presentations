a37e4fae28ece180232b10244f1408dc90b532fd
Sophie Gill <sophie-gill@hotmail.com>
2016-07-14 09:47:24 +0100

    adding default state to traffic light

--- four-phase-testing/traffic_light/lib/traffic_light.rb
+++ four-phase-testing/traffic_light/lib/traffic_light.rb
@@ -1,3 +1,13 @@
 class TrafficLight
   #the awesome code goes here
+  attr_reader :state
+
+  def initialize(state = :stop)
+    @state = :stop
+  end
+
+  def next
+    true
+  end
+
 end
--- four-phase-testing/traffic_light/spec/traffic_light_spec.rb
+++ four-phase-testing/traffic_light/spec/traffic_light_spec.rb
@@ -12,4 +12,16 @@ describe TrafficLight do
 #teardown (you will probably not need this)
   end
 
+  describe '#state' do
+    it 'is set to "stop" by default' do
+      expect(subject.state).to(eq(:stop))
+    end
+  end
+
+  describe '#next' do
+    it 'responds to next' do
+      expect(subject.next).to(eq(true))
+    end
+  end
+
 end
